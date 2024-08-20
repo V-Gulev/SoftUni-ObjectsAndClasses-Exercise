c83aa221e2a9499a3a63cb812e866d1ffabd3f83
V-Gulev <velizargulev@abv.bg>
2024-08-20 17:27:25 +0300

    Randomize Words

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
+import java.util.*;
+
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] words = input.split(" ");
+        Random rnd = new Random();
+
+        for (int i = 0; i < words.length; i++) {
+            int random = rnd.nextInt(words.length);
+            String temporary = words[i];
+            words[i] = words[random];
+            words[random] = temporary;
+        }
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
 }
