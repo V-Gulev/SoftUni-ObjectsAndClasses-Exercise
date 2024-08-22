a3c7fd0222cfcfb1547e21bdf050b3ebd061285d
V-Gulev <velizargulev@abv.bg>
2024-08-22 18:23:31 +0300

    Teamwork projects

--- src/Main.java
+++ src/Main.java
@@ -1,116 +1,132 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class Main {
-    public static class Engine {
-        String model;
-        int power;
-        String displacement;
-        String efficiency;
+    public static class Team {
+        private String name;
+        private String creator;
+        private List<String> members;
 
-        public Engine(String model, int power) {
-            this.model = model;
-            this.power = power;
-            this.displacement = "n/a";
-            this.efficiency = "n/a";
+        public Team(String name, String creator) {
+            this.name = name;
+            this.creator = creator;
+            this.members = new ArrayList<>();
         }
 
-        public Engine(String model, int power, String displacement) {
-            this.model = model;
-            this.power = power;
-            this.displacement = displacement;
-            this.efficiency = "n/a";
+
+        public String getName() {
+            return name;
         }
 
-        public Engine(String model, int power, String displacement, String efficiency) {
-            this.model = model;
-            this.power = power;
-            this.displacement = displacement;
-            this.efficiency = efficiency;
-        }
-        public String toString() {
-            return String.format("  %s:%n    Power: %d%n    Displacement: %s%n    Efficiency: %s",
-                    model, power, displacement, efficiency);
-        }
-    }
-    public static class Car {
-        String model;
-        Engine engine;
-        String weight;
-        String colour;
-
-        public Car(String model, Engine engine) {
-            this.model = model;
-            this.engine = engine;
-            this.weight = "n/a";
-            this.colour = "n/a";
+        public String getCreator() {
+            return creator;
         }
 
-        public Car(String model, Engine engine, String weight) {
-            this.model = model;
-            this.engine = engine;
-            this.weight = weight;
-            this.colour = "n/a";
+        public List<String> getMembers() {
+            return members;
         }
 
-        public Car(String model, Engine engine, String weight, String colour) {
-            this.model = model;
-            this.engine = engine;
-            this.weight = weight;
-            this.colour = colour;
+        public void addMember(String member) {
+            this.members.add(member);
+        }
+
+        public int getMemberCount() {
+            return this.members.size();
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setCreator(String creator) {
+            this.creator = creator;
+        }
+
+        public void setMembers(List<String> members) {
+            this.members = members;
         }
 
         public String toString() {
-            return String.format("%s:%n%s%n  Weight: %s%n  Color: %s",
-                    model, engine.toString(), weight, colour);
+            StringBuilder sb = new StringBuilder();
+            sb.append(name).append("\n");
+            sb.append("- ").append(creator).append("\n");
+            for (String member : members) {
+                sb.append("-- ").append(member).append("\n");
+            }
+            return sb.toString();
         }
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-        Map<String, Engine> engineMap = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            String[] input = scanner.nextLine().split(" ");
-            String model = input[0];
-            int power = Integer.parseInt(input[1]);
-            Engine engine;
-            if (input.length == 2) {
-                engine = new Engine(model,power);
-            } else if (input.length == 3) {
-                String data = input[2];
-                if (Character.isDigit(data.charAt(0))) {
-                    engine = new Engine(model,power, data);
-                } else {
-                    engine = new Engine(model, power, "n/a", data);
-                }
+        int teamCount = Integer.parseInt(scanner.nextLine());
+        Map<String, Team> teams = new HashMap<>();
+        Set<String> users = new HashSet<>();
+
+        for (int i = 0; i < teamCount; i++) {
+            String[] teamInfo = scanner.nextLine().split("-");
+            String user = teamInfo[0];
+            String teamName = teamInfo[1];
+
+            if (teams.containsKey(teamName)) {
+                System.out.printf("Team %s was already created!%n", teamName);
+            } else if (users.contains(user)) {
+                System.out.printf("%s cannot create another team!%n", user);
             } else {
-                engine = new Engine(model, power, input[2], input[3]);
+                Team team = new Team(teamName, user);
+                teams.put(teamName, team);
+                users.add(user);
+                System.out.printf("Team %s has been created by %s!%n", teamName, user);
             }
-            engineMap.put(model, engine);
         }
 
-        int m = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 0; i < m; i++) {
-            String[] input = scanner.nextLine().split(" ");
-            String model = input[0];
-            Engine engine = engineMap.get(input[1]);
-            Car car;
-            if (input.length == 2) {
-                car = new Car(model,engine);
-            } else if (input.length == 3) {
-                String data = input[2];
-                if (Character.isDigit(data.charAt(0))) {
-                    car = new Car(model, engine, data, "n/a");
+        String line = scanner.nextLine();
+        while (!line.equals("end of assignment")) {
+            String[] joinInfo = line.split("->");
+            String user = joinInfo[0];
+            String teamName = joinInfo[1];
+
+            if (!teams.containsKey(teamName)) {
+                System.out.printf("Team %s does not exist!%n", teamName);
+            } else {
+                boolean alreadyInTeam = teams.values().stream().anyMatch(t -> t.getMembers().contains(user) || t.getCreator().equals(user));
+
+                if (alreadyInTeam) {
+                    System.out.printf("Member %s cannot join team %s!%n", user, teamName);
                 } else {
-                    car = new Car(model,engine,"n/a", data);
+                    teams.get(teamName).addMember(user);
                 }
-            }  else {
-                car = new Car(model, engine, input[2], input[3]);
             }
-            System.out.println(car);
+
+            line = scanner.nextLine();
+        }
+
+
+        List<Team> activeTeams = new ArrayList<>();
+        List<String> disbandTeams = new ArrayList<>();
+
+        for (Team team : teams.values()) {
+            if (team.getMemberCount() > 0) {
+                activeTeams.add(team);
+            } else {
+                disbandTeams.add(team.getName());
+            }
+        }
+
+
+        activeTeams.sort(Comparator.comparing(Team::getMemberCount).reversed().thenComparing(Team::getName));
+
+        for (Team team : activeTeams) {
+            team.getMembers().sort(String::compareTo);
+            System.out.println(team);
+        }
+
+
+        System.out.println("Teams to disband:");
+        disbandTeams.sort(String::compareTo);
+        for (String teamName : disbandTeams) {
+            System.out.println(teamName);
         }
     }
 }
