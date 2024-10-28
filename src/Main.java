4a9ca88a8367a130fb7ba37768ca7ff53965332d
V-Gulev <velizargulev@abv.bg>
2024-10-28 18:06:36 +0200

    Article

--- src/Main.java
+++ src/Main.java
@@ -1,132 +1,35 @@
-import java.util.*;
-
+import java.util.Scanner;
 
 public class Main {
-    public static class Team {
-        private String name;
-        private String creator;
-        private List<String> members;
-
-        public Team(String name, String creator) {
-            this.name = name;
-            this.creator = creator;
-            this.members = new ArrayList<>();
-        }
-
-
-        public String getName() {
-            return name;
-        }
-
-        public String getCreator() {
-            return creator;
-        }
-
-        public List<String> getMembers() {
-            return members;
-        }
-
-        public void addMember(String member) {
-            this.members.add(member);
-        }
-
-        public int getMemberCount() {
-            return this.members.size();
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public void setCreator(String creator) {
-            this.creator = creator;
-        }
-
-        public void setMembers(List<String> members) {
-            this.members = members;
-        }
-
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            sb.append(name).append("\n");
-            sb.append("- ").append(creator).append("\n");
-            for (String member : members) {
-                sb.append("-- ").append(member).append("\n");
-            }
-            return sb.toString();
-        }
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int teamCount = Integer.parseInt(scanner.nextLine());
-        Map<String, Team> teams = new HashMap<>();
-        Set<String> users = new HashSet<>();
+        Scanner sc = new Scanner(System.in);
+        String[] parts = sc.nextLine().split(", ");
+        String title = parts[0];
+        String content = parts[1];
+        String author = parts[2];
+        Article article1 = new Article(title,content,author);
 
-        for (int i = 0; i < teamCount; i++) {
-            String[] teamInfo = scanner.nextLine().split("-");
-            String user = teamInfo[0];
-            String teamName = teamInfo[1];
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < n; i++) {
+            String[] elements = sc.nextLine().split(": ");
+            String command = elements[0];
 
-            if (teams.containsKey(teamName)) {
-                System.out.printf("Team %s was already created!%n", teamName);
-            } else if (users.contains(user)) {
-                System.out.printf("%s cannot create another team!%n", user);
-            } else {
-                Team team = new Team(teamName, user);
-                teams.put(teamName, team);
-                users.add(user);
-                System.out.printf("Team %s has been created by %s!%n", teamName, user);
+            switch (command) {
+                case "Edit":
+                    article1.edit(elements[1]);
+                    break;
+                case "ChangeAuthor":
+                    article1.changeAuthor(elements[1]);
+                    break;
+                case "Rename":
+                    article1.rename(elements[1]);
+                    break;
+                default:
+                    break;
+
             }
         }
+        System.out.println(article1);
 
-
-        String line = scanner.nextLine();
-        while (!line.equals("end of assignment")) {
-            String[] joinInfo = line.split("->");
-            String user = joinInfo[0];
-            String teamName = joinInfo[1];
-
-            if (!teams.containsKey(teamName)) {
-                System.out.printf("Team %s does not exist!%n", teamName);
-            } else {
-                boolean alreadyInTeam = teams.values().stream().anyMatch(t -> t.getMembers().contains(user) || t.getCreator().equals(user));
-
-                if (alreadyInTeam) {
-                    System.out.printf("Member %s cannot join team %s!%n", user, teamName);
-                } else {
-                    teams.get(teamName).addMember(user);
-                }
-            }
-
-            line = scanner.nextLine();
-        }
-
-
-        List<Team> activeTeams = new ArrayList<>();
-        List<String> disbandTeams = new ArrayList<>();
-
-        for (Team team : teams.values()) {
-            if (team.getMemberCount() > 0) {
-                activeTeams.add(team);
-            } else {
-                disbandTeams.add(team.getName());
-            }
-        }
-
-
-        activeTeams.sort(Comparator.comparing(Team::getMemberCount).reversed().thenComparing(Team::getName));
-
-        for (Team team : activeTeams) {
-            team.getMembers().sort(String::compareTo);
-            System.out.println(team);
-        }
-
-
-        System.out.println("Teams to disband:");
-        disbandTeams.sort(String::compareTo);
-        for (String teamName : disbandTeams) {
-            System.out.println(teamName);
-        }
     }
 }
